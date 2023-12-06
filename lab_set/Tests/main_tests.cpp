a7397c3e5f5ca6445e2d51dae7587a2110ce4bc8
Dudchenko Olesya <dudchenkoleska@gmail.com>
2023-12-06 04:14:39 +0300

    tests doesnt run

---
+++ lab_set/Tests/main_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright 2023 Dudchenko Olesya
+
+#ifndef _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+
+#include "../Gtests/gtest.h"
+#include "../Tests/test_bitfield.h"
+#include "../Tests/test_set.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    system("pause");
+    return 0;
+}
+#endif _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
